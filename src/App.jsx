502397635a2ad22dea3ae908e9ff20e823d7f6db
OLOWONISHAYE-SUNKANMI <skprojectx12@gmail.com>
2025-08-12 09:49:39 -0700

    newww

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-
+import {
+  createRoutesFromElements,
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './App.css'
+import Home from './pages/homepage/homepage.jsx'  
+
 
 function App() {
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+         <Route path="/" element={<Home />} />
+     )
+  );
+
   return (
     <>
-      <p className="text-4xl">Working</p>
+      <RouterProvider router={router} />
     </>
   )
 }
