419bc56846aa509ff7afbf3e581b96fa37cded78
OLOWONISHAYE-SUNKANMI <skprojectx12@gmail.com>
2025-08-14 01:53:39 -0700

    newwww

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Hero from '../../components/hero/hero';
-
+import Info from '../../components/info/info';
 
 
 export default function homepage() {
   return (
-  
    <>
-    <div className='w-full h-100'>
+    <div className='w-full h-80'>
      <Hero />
+    </div>
+    <div> 
+       <Info />
     </div>
    </>
   )
