82de70a62ce65095592a7078f0fd8595078462b1
OLOWONISHAYE-SUNKANMI <skprojectx12@gmail.com>
2025-08-14 05:06:11 -0700

    newwwww

--- src/components/info/info.jsx
+++ src/components/info/info.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function info() {
   return (
-    <div>info</div>
+    <>
+    <div className='flex justify-center py-10'>
+        <div className='py-10 mr-10'>
+          <img src='/charlie marie/IMG-20250812-WA0045.jpg' alt='info' className='w-2/5'/>
+      </div>
+
+    </div>
+      
+    </>
   )
 }
