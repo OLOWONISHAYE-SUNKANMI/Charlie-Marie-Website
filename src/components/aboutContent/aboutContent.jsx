d11921da19aeae3a74a6d875f00872b0f1d1fdbc
OLOWONISHAYE-SUNKANMI <skprojectx12@gmail.com>
2025-08-18 22:00:54 -0700

    new changes

--- src/components/aboutContent/aboutContent.jsx
+++ src/components/aboutContent/aboutContent.jsx
@@ -1,33 +1,44 @@
-import React from 'react'
-import { Fade } from 'react-awesome-reveal'
 
-export default function aboutContent() {
+import { Slide, Fade } from 'react-awesome-reveal';
+
+export default function Info() {
   return (
-    <div className='w-full h-[100vh]'>
-        <Fade direction='down' triggerOnce>
-            <div className='flex py-4 px-8 '>
+    <div className='container mx-auto px-2 pt-10'>
+      <div className='flex flex-col lg:flex-row justify-center items-center'>
 
-            <div className='justify-center items-center'>
-                <h3 className='p-5 bg-[#620000] text-white text-center w-98  mx-10 my-10 font-extrabold text-2xl'>
-                    MISSION STATEMENT
-                </h3>
-                <p className='text-bold text-xl p-10'>
-                    We are committed to building a brighter future for prosperity. Education is our priority till all our Children are mature
-                </p>
-            </div>
-
-             <div className='justify-center items-center'>
-                <h3 className='p-5 bg-[#620000] text-white text-center mx-10 my-10 font-extrabold text-2xl'>
-                    OUR AIMS AND OBJECTIVES
-                </h3>
-                <p>
-                    We are committed to building a brighter future for prosperity. Education is our priority till all our Children are mature
-                </p>
-            </div>
-
-            </div>
-            
+           {/* Image Section - Visible on all screens */}
+        <Fade direction='top-left'>
+          <div className='w-full lg:w-auto mb-10 lg:mb-0 lg:py-5 lg:mr-10 flex justify-center'>
+            <img 
+              src='https://res.cloudinary.com/depeqzb6z/image/upload/v1755517513/IMG-20250812-WA0075_lz4tay.jpg' 
+              alt='info' 
+              className='w-full max-w-md lg:m-20 lg:w-auto shadow-md'
+              style={{ maxHeight: '500px' }} // Adjust height as needed
+            />
+          </div>
         </Fade>
+     
+        {/* Content Section */}
+        <Slide direction='down'>
+          <div className='w-full lg:w-4/5'>
+            <h1 className='font-extrabold font-poppins text-2xl lg:text-3xl text-[#620000] text-center'>Mission Statement</h1>
+            <p className='mt-5 lg:mt-10 text-sm lg:text-base mb-5 text-center'>
+              We are committed to buillding a brighter future for posterity. Education is our priority till all our Children are mature.
+            </p>
+
+             <h1 className='font-extrabold font-poppins text-2xl lg:text-3xl text-[#115237] text-center'>Our Aims and Objectives</h1>
+            <ul className='mt-5 mb-5'>
+                <li className='mt-5 lg:mt-10 text-sm lg:text-base mb-5 '>1. We aim to provide adequate care and supervision for our children from very tender ages.</li>
+                <li className='mt-5 lg:mt-10 text-sm lg:text-base mb-5 '>2. We aim to give our children sound knowledge and prepare them for advanced education in later years by paying them individual attention.</li>
+                <li className='mt-5 lg:mt-10 text-sm lg:text-base mb-5 '>3.  We shall encourage the child's social development through interaction with other children.</li>
+                <li className='mt-5 lg:mt-10 text-sm lg:text-base mb-5 '>4. We shall ensure that all our children are well prepared for higher education with ease and confidence to achieve excellence and greatness.</li>
+                <li className='mt-5 lg:mt-10 text-sm lg:text-base mb-5 '>5. We are resolved to produce well educated. well behaved, and honest boys and girls for the future.</li>
+                <p className='mt-5 lg:mt-10 text-sm lg:text-base mb-5 '>So help us God. Amen</p>
+            </ul>
+          </div>
+
+        </Slide>
+      </div>
     </div>
   )
 }
--- src/components/aboutHero/aboutHero.jsx
+++ src/components/aboutHero/aboutHero.jsx
@@ -12,7 +12,7 @@ export default function aboutHero() {
                                 md:px-10 md:py-14 
                                 lg:px-20 lg:py-16
                                 xl:px-32 xl:py-20">
-                  <p className="font-extrabold text-[#115237] my-8
+                  <p className="font-extrabold text-[#FFFFFF] my-8
                                  text-2xl sm:text-3xl 
                                  md:text-4xl lg:text-5xl
                                  whitespace-nowrap overflow-x-auto pb-2
